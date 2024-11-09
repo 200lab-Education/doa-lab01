f9aad9a415750fc7c773226927ac1d79a12ca870
NghiaTran <63591554+nghiatrann0502@users.noreply.github.com>
2024-11-09 21:48:00 +0700

    .

--- src/component/s3_uploader.ts
+++ src/component/s3_uploader.ts
@@ -1,13 +1,13 @@
-import { S3Client } from "@aws-sdk/client-s3";
-import { Upload } from "@aws-sdk/lib-storage";
-import { IImageUploader } from "@share/interface";
-import { config } from "dotenv";
-import fs from "fs";
+import { S3Client } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
+import { IImageUploader } from '@share/interface'
+import { config } from 'dotenv'
+import fs from 'fs'
 
-config();
+config()
 
 export class S3Uploader implements IImageUploader {
-  constructor() { }
+  constructor() {}
 
   async uploadImage(objName: string, filename: string, filesize: number, contentType: string): Promise<boolean> {
     const parallelUploads3 = new Upload({
@@ -25,18 +25,18 @@ export class S3Uploader implements IImageUploader {
       queueSize: 4, // optional concurrency configuration
       partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
       leavePartsOnError: false // optional manually handle dropped parts
-    });
+    })
 
-    await parallelUploads3.done();
-    return true;
+    await parallelUploads3.done()
+    return true
   }
 
   cloudName(): string {
-    return 'aws-s3';
+    return 'aws-s3'
   }
 
   getFullURL(objectName: string): string {
-    return process.env.URL_PUBLIC + '/' + objectName;
+    return process.env.URL_PUBLIC + '/' + objectName
   }
 }
 
@@ -46,4 +46,4 @@ export const s3 = new S3Client({
     secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY as string,
     accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID as string
   }
-});
+})
